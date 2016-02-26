35c1e12afe0abe5c5a6f68259dc2051e67d8733f
Michael Readhead <michael.readhead@trackit-solutions.com>
2016-02-26 09:37:46 +0000

    Styling changes largely.

--- scraper.py
+++ scraper.py
@@ -2,45 +2,57 @@ import requests, Queue, os, getpass, urllib
 from threading import Thread
 from bs4 import BeautifulSoup
 
-def saveImage(queue):
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
+def save_image(queue):
     index = 1
     size = queue.qsize()
     while not queue.empty():
-        print ("Downloading [{}/{}]".format(index, size))
+        print ('Downloading [{}/{}]'.format(index, size))
         image = queue.get()
         urllib.urlretrieve(image, os.path.basename(image))
         index += 1
-        
-def createFolder():
-    folderName = raw_input("What do you want to call your folder Name? ")
-            #creates folder if it does not exist
-    if os.path.exists("C:\Users\\" + getpass.getuser() + "\Pictures\\StanceNation\\" + folderName):
-        os.chdir("C:\Users\\" + getpass.getuser() + "\Pictures\\StanceNation\\" + folderName)
+
+
+def create_folder():
+    folder_name = input('What do you want to call your folder Name? ')
+    # Creates folder if it does not exist
+    folder_string = 'C:\\Users\\{}\\Pictures\\StanceNation\\{}'.format(
+        getpass.getuser(), folder_name)
+
+    if os.path.exists(folder_string):
+        os.chdir(folder_string)
     else:
-        os.makedirs("C:\Users\\" + getpass.getuser() + "\Pictures\\StanceNation\\" + folderName)
-        os.chdir("C:\Users\\" + getpass.getuser() + "\Pictures\\StanceNation\\" + folderName)     
+        os.makedirs(folder_string)
+        os.chdir(folder_string)
 
-ans = 'y'
-while ans == 'y':
-    url = raw_input("What is the StanceNation link?\n")
-    if 'http://' not in url:
-        url = 'http://' + url
-        
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")    
-    
-    queue = Queue.LifoQueue()
-    
-    createFolder()
-    
-    for image in soup.findAll('meta', {"property":'og:image'}):            
-        if '1140' or '1500' in image['content']:
-            queue.put(image['content']) 
 
-    for i in range(10):
-        thread_num_1 = Thread(saveImage(queue))
-        thread_num_2 = Thread(saveImage(queue)) 
-        thread_num_1.start()
-        thread_num_2.start()
-        
-    ans = raw_input("Do you have another link?(y/n)\n")       
+def main():
+    ans = 'y'
+    while ans == 'y':
+        url = input('What is the StanceNation link?\n')
+        if 'http://' not in url:
+            url = 'http://' + url
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        queue = Queue.LifoQueue()
+        create_folder()
+        for image in soup.findAll('meta', {'property': 'og:image'}):
+            if '1140' or '1500' in image['content']:
+                queue.put(image['content'])
+
+        for i in range(10):
+            thread_num_1 = Thread(save_image(queue))
+            thread_num_2 = Thread(save_image(queue))
+            thread_num_1.start()
+            thread_num_2.start()
+
+        ans = raw_input('Do you have another link?(y/n)\n')
+
+if __name__ == '__main__':
+    main()
