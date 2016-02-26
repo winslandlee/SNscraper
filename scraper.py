8ef8084c257928adba219c19352ea7ba087b4d4a
Winsland Lee <winslandlee@gmail.com>
2016-02-25 23:57:26 -0500

    Reformatted Code into one script
    implemented multithreading and queues

--- scraper.py
+++ scraper.py
@@ -1,30 +1,46 @@
-import requests
-import Queue
-import getLinks
-import saveImages
+import requests, Queue, os, getpass, urllib
 from threading import Thread
 from bs4 import BeautifulSoup
 
-queue = Queue.Queue()
+def saveImage(queue):
+    index = 1
+    size = queue.qsize()
+    while not queue.empty():
+        print ("Downloading [{}/{}]".format(index, size))
+        image = queue.get()
+        urllib.urlretrieve(image, os.path.basename(image))
+        index += 1
+        
+def createFolder():
+    folderName = raw_input("What do you want to call your folder Name? ")
+            #creates folder if it does not exist
+    if os.path.exists("C:\Users\\" + getpass.getuser() + "\Pictures\\StanceNation\\" + folderName):
+        os.chdir("C:\Users\\" + getpass.getuser() + "\Pictures\\StanceNation\\" + folderName)
+    else:
+        os.makedirs("C:\Users\\" + getpass.getuser() + "\Pictures\\StanceNation\\" + folderName)
+        os.chdir("C:\Users\\" + getpass.getuser() + "\Pictures\\StanceNation\\" + folderName)     
 
-url = raw_input("What is the StanceNation link?\n")
-if 'http://' not in url:
-    url = 'http://' + url
+ans = 'y'
+while ans == 'y':
+    url = raw_input("What is the StanceNation link?\n")
+    if 'http://' not in url:
+        url = 'http://' + url
+        
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")    
     
-response = requests.get(url)
-data = response.text
-soup = BeautifulSoup(data, "html.parser")
+    queue = Queue.LifoQueue()
+    
+    createFolder()
+    
+    for image in soup.findAll('meta', {"property":'og:image'}):            
+        if '1140' or '1500' in image['content']:
+            queue.put(image['content']) 
 
-pages = getLinks.getPages(soup)
-
-saveImages.makeDir()
-
-getLinks.imageParse(url, soup, queue)
-
-for i in range(10):
-    thread = Thread(saveImages.saveImage(queue))
-    thread.start()
-
-queue.join()
-
-print ("All Images Downloaded")
+    for i in range(10):
+        thread_num_1 = Thread(saveImage(queue))
+        thread_num_2 = Thread(saveImage(queue)) 
+        thread_num_1.start()
+        thread_num_2.start()
+        
+    ans = raw_input("Do you have another link?(y/n)\n")       
