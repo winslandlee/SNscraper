2b05aa76776f26e965f69d05c69c306d3b7aa4f3
Mike Readhead <michael.readhead@trackit-solutions.com>
2016-03-07 10:35:16 +0000

    Shuffle.

--- scraper.py
+++ scraper.py
@@ -1,62 +1,67 @@
-import os, getpass, urllib
-# from threading import Thread
-from multiprocessing import Process, JoinableQueue
-#   Process is almost identical to Thread and can be used in it's place.
-#   We can also use the multiprocessing Queue.
-
-from bs4 import BeautifulSoup
 import requests
-try:
-    input = raw_input
-except NameError:
-    pass
+import urllib
+import os
+from multiprocessing import cpu_count, JoinableQueue, Process
+from bs4 import BeautifulSoup
+
+
+FOLDER_PATH_FORMAT_STRING = "C:\Users\\{}\\Pictures\\StanceNation\\{}"
+
+
+def get_links(queue):
+    url = raw_input("What is the StanceNation link?\n")
+    if 'http://' not in url:
+        url = 'http://' + url
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    for image in soup.findAll('meta', {"property": 'og:image'}):
+        if '1140' or '1500' in image['content']:
+            queue.put(image['content'])
 
 
 def save_image(queue):
     index = 1
     size = queue.qsize()
     while not queue.empty():
-        print ('Downloading [{}/{}]'.format(index, size))
+        print ("Downloading [{}/{}]".format(index, size))
         image = queue.get()
         urllib.urlretrieve(image, os.path.basename(image))
         index += 1
 
 
 def create_folder():
-    folder_name = input('What do you want to call your folder Name? ')
-    # Creates folder if it does not exist
-    folder_string = 'C:\\Users\\{}\\Pictures\\StanceNation\\{}'.format(
-        getpass.getuser(), folder_name)
-
-    if os.path.exists(folder_string):
-        os.chdir(folder_string)
+    folder_name = raw_input("What do you want to call your folder Name? ")
+    # creates folder if it does not exist
+    # I'm in linux so I'm ignoreing this bit....
+    # but
+    # path = FOLDER_PATH_FORMAT_STRING.format(username, folder_name)
+    if os.path.exists(folder_name):
+        os.chdir(folder_name)
     else:
-        os.makedirs(folder_string)
-        os.chdir(folder_string)
+        os.makedirs(folder_name)
+        os.chdir(folder_name)
 
 
 def main():
+    processes = cpu_count() * 2
+    queue = JoinableQueue()
     ans = 'y'
+
     while ans == 'y':
-        url = input('What is the StanceNation link?\n')
-        if 'http://' not in url:
-            url = 'http://' + url
 
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        queue = JoinableQueue()
+        get_links(queue)
         create_folder()
-        for image in soup.findAll('meta', {'property': 'og:image'}):
-            if '1140' or '1500' in image['content']:
-                queue.put(image['content'])
 
-        for i in range(10):
-            thread_num_1 = Thread(save_image(queue))
-            thread_num_2 = Thread(save_image(queue))
-            thread_num_1.start()
-            thread_num_2.start()
+        for i in range(processes):
+            p = Process(target=save_image, args=(queue,)).start()
 
-        ans = raw_input('Do you have another link?(y/n)\n')
+        queue.join()
+        queue.close()
 
-if __name__ == '__main__':
+        ans = raw_input("Do you have another link?(y/n)\n")
+
+
+if __name__ == "__main__":
     main()
